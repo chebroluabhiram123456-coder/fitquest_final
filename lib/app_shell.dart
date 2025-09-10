1402381a57624ef55496b5827114eaa3b0c8d142
chebroluabhiram123456-coder <chebroluabhiram123456@gmail.com>
2025-09-10 14:44:49 +0530

    Create app_shell.dart

---
+++ lib/app_shell.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+import 'screens/progress_screen.dart';
+import 'screens/plan_screen.dart';
+import 'screens/profile_screen.dart';
+
+class AppShell extends StatefulWidget {
+  const AppShell({super.key});
+
+  @override
+  State<AppShell> createState() => _AppShellState();
+}
+
+class _AppShellState extends State<AppShell> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomeScreen(),
+    ProgressScreen(),
+    PlanScreen(),
+    ProfileScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color(0xFF2C153D),
+              Color(0xFF1A0A24),
+            ],
+          ),
+        ),
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.show_chart),
+            label: 'Progress',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Plan',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white.withOpacity(0.6),
+        onTap: _onItemTapped,
+        backgroundColor: const Color(0xFF1A0A24),
+        type: BottomNavigationBarType.fixed,
+      ),
+    );
+  }
+}
